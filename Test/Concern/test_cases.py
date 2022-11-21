b652bdcbf09d1d0d97dc52306c49f116c58f144f
EarlArcebido15 <ejarcebido@cheqsystems.com>
2022-11-21 12:14:01 +0800

    Updated TestCases

--- Test/Concern/test_cases.py
+++ Test/Concern/test_cases.py
@@ -7,6 +7,8 @@ from Pages.Page_CXE_Page_Search import CXESearchPage
 from Pages.Page_CXE_Question_Response import CXEQuestionResponsePage
 from Pages.Page_CXE_Question_Responses import CXEQuestionResponsesPage
 from Pages.Page_Yopmail_Home import YopmailHomePage
+from Pages.Page_Home import HomePage
+from Pages.Page_ContactUs import ContactUsPage
 from Test.Module_Functions.MO_Functions import *
 from Utilities.Functions import *
 from Utilities.Utils import Utilities
@@ -25,3 +27,45 @@ customer_document = CXECustomerDocumentPage()
 int_outage = InternalOutagePage()
 document = CXEDocumentPage()
 module = "Concern"
+inquiry_reference_no = ''
+
+def TC001(driver,ts_id):
+    test_case = "TC001"
+    homePage = HomePage()
+    contactUsPage = ContactUsPage()
+
+    fc.bookmark(module, ts_id, test_case, "Step 1")
+    fc.click(homePage.get_contact_us(driver))
+    fc.click(contactUsPage.get_log_inquiry(driver))
+    fc.scroll_element(driver, contactUsPage.get_log_remarks(driver))
+    fc.screen_capture(driver, module, ts_id, test_case, "Step 1")
+
+def TC005(driver, ts_id):
+    test_case = "TC005"
+    contactUsPage = ContactUsPage()
+
+    fc.bookmark(module, ts_id, test_case, "Step 1")
+    fc.select_dropdown_element(driver,contactUsPage.get_log_inquiry_concern_type(driver), Outage['inquiry_concern_type'])
+    contactUsPage.enter_remarks(driver,Concern['inquiry_remarks'])
+    fc.scroll_element(driver, contactUsPage.get_log_inquiry_submit(driver))
+    fc.screen_capture(driver, module, ts_id, test_case, "Step 1")
+
+    fc.bookmark(module, ts_id, test_case, "Step 2")
+    fc.click(contactUsPage.get_log_inquiry_submit(driver))
+    fc.scroll_element(driver, contactUsPage.get_log_inquiry(driver))
+    inquiry_reference_no = contactUsPage.get_log_inquiry_reference_number(driver)
+    fc.screen_capture(driver, module, ts_id, test_case, "Step 2")
+
+def TC006(driver, ts_id):
+    test_case = "TC005"
+    contactUsPage = ContactUsPage()
+
+    fc.bookmark(module, ts_id, test_case, "Step 1")
+
+
+
+
+
+
+
+
